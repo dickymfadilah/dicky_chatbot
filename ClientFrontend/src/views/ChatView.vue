db08786536a3b08d32f81165c39dc1ea44ea808d
dickymfadilah <dicky@trillium-tech.com>
2025-03-13 15:25:43 +0700

    fix

--- ClientFrontend/src/views/ChatView.vue
+++ ClientFrontend/src/views/ChatView.vue
@@ -164,7 +164,7 @@ export default {
     
     const showDatabaseInfo = async () => {
       try {
-        userInput.value = "Show me the available database collection and what kind of data they contain."
+        userInput.value = "Show me the available database"
         await handleSendMessage()
       } catch (error) {
         console.error('Failed to query database:', error)
